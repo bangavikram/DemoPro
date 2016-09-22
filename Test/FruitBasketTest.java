5d9ed0657520e592a22281c7af8a74816b1abc32
Vikram Banga <vikrambanga8559@gmail.com>
2016-09-22 23:19:39 +0530

    New Project
    New Project

---
+++ Test/FruitBasketTest.java
@@ -0,0 +1,126 @@
+package com.fruit.test;
+
+import static org.junit.Assert.*;
+
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.fruit.exception.EmptyBasketException;
+import com.one.fruit.FruitBasket;
+import com.one.fruit.FruitConstants;
+
+public class FruitBasketTest {
+
+	private HashMap<String, Integer> mapFruit=null;
+	private FruitBasket fruitBasket = null;
+	
+		@Before  
+	   public void setUp() throws Exception {
+		
+		mapFruit = new HashMap<String,Integer>();
+		fruitBasket=new FruitBasket();
+	    } 
+
+	
+		@Test
+		public void testGetBasketValueWithAllFruits() throws EmptyBasketException {
+			mapFruit.put(FruitConstants.FRUIT_LEMON, 2);
+			mapFruit.put(FruitConstants.FRUIT_BANANA, 1);
+			mapFruit.put(FruitConstants.FRUIT_ORANGE,3);
+			mapFruit.put(FruitConstants.FRUIT_APPLE,2);
+			mapFruit.put(FruitConstants.FRUIT_PEACH, 5);
+			 
+			 float actualValue=fruitBasket.calculateBasketValue(mapFruit);
+			 float expectedValue=119.0f;
+
+			 assertNotNull(actualValue);
+			 assertEquals(expectedValue, actualValue, 0);
+		}
+		
+		@Test
+		public void testGetBasketValueWithFourFruits() throws EmptyBasketException {
+			mapFruit.put(FruitConstants.FRUIT_LEMON, 2);
+			mapFruit.put(FruitConstants.FRUIT_BANANA, 5);
+			mapFruit.put(FruitConstants.FRUIT_APPLE,2);
+			mapFruit.put(FruitConstants.FRUIT_PEACH, 1);
+			 
+			 float actualValue=fruitBasket.calculateBasketValue(mapFruit);
+			 float expectedValue=73.0f;
+
+			 assertNotNull(actualValue);
+			 assertEquals(expectedValue, actualValue, 0);
+		}
+		
+		@Test
+		public void testGetBasketValueWithThreeFruits() throws EmptyBasketException {
+			mapFruit.put(FruitConstants.FRUIT_ORANGE,2);
+			mapFruit.put(FruitConstants.FRUIT_APPLE,2);
+			mapFruit.put(FruitConstants.FRUIT_PEACH,3);
+			 
+			 float actualValue=fruitBasket.calculateBasketValue(mapFruit);
+			 float expectedValue=68.0f;
+
+			 assertNotNull(actualValue);
+			 assertEquals(expectedValue, actualValue, 0);
+		}
+		
+		@Test
+		public void testGetBasketValueWithTwoFruits() throws EmptyBasketException {
+			mapFruit.put(FruitConstants.FRUIT_LEMON, 2);
+			mapFruit.put(FruitConstants.FRUIT_BANANA, 4);
+			 
+			 float actualValue=fruitBasket.calculateBasketValue(mapFruit);
+			 float expectedValue=36.0f;
+
+			 assertNotNull(actualValue);
+			 assertEquals(expectedValue, actualValue, 0);
+		}
+		
+		@Test
+		public void testGetBasketValueWithOneFruit() throws EmptyBasketException {
+			
+			mapFruit.put(FruitConstants.FRUIT_PEACH, 3);
+			 
+			 float actualValue=fruitBasket.calculateBasketValue(mapFruit);
+			 float expectedValue=36.0f;
+
+			 assertNotNull(actualValue);
+			 assertEquals(expectedValue, actualValue, 0);
+		}
+	
+		
+		@Test(expected=EmptyBasketException.class)
+		public void testGetBasketValueWithNoFruit() throws EmptyBasketException {
+			
+			 
+			 float actualValue=fruitBasket.calculateBasketValue(mapFruit);
+			 float expectedValue=0.0f;
+
+			 assertNotNull(actualValue);
+			 assertEquals(expectedValue, actualValue, 0);
+		}
+		
+		@Test
+		public void testGetBasketValueWithNegativeValue() throws EmptyBasketException {
+			
+			mapFruit.put(FruitConstants.FRUIT_LEMON, -2);
+			mapFruit.put(FruitConstants.FRUIT_BANANA, -1);
+			 
+			 float actualValue=fruitBasket.calculateBasketValue(mapFruit);
+			 float expectedValue=0.0f;
+
+			 assertNotNull(actualValue);
+			 assertEquals(expectedValue, actualValue, 0);
+		}
+		
+		@After
+		public void tearDown() throws Exception {  
+			mapFruit=null;  
+			fruitBasket=null;
+		} 
+
+
+}
---
+++ src/com/one/fruit/FruitConstants.java
@@ -0,0 +1,16 @@
+package com.one.fruit;
+
+public class FruitConstants {
+	
+	public static final String FRUIT_BANANA = "Bananas";
+	public static final String FRUIT_APPLE = "Apples";
+	public static final String FRUIT_LEMON = "Lemons";
+	public static final String FRUIT_PEACH = "Peaches";
+	public static final String FRUIT_ORANGE = "Oranges";
+	
+	public static final float BANANA_PRICE = 5f;
+	public static final float APPLE_PRICE = 10f;
+	public static final float ORANGE_PRICE = 6f;
+	public static final float PEACH_PRICE = 12f;
+	public static final float LEMON_PRICE = 8f;
+}
